f161e6d6cd1a9c61fe35765d57584ce13a5b4cac
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-29 01:12:39 +0800

    Add tailwindcss configuration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
-import cjcLogo from "./assets/cjc-logo.png"
+import cjcLogo from "./assets/cjc-logo.png";
+import "./index.css";
 
 function App() {
   return (
     <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
