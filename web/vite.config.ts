d85d3df443290af7ee62a362c5358fa79ab67f4b
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-30 00:28:35 +0800

    Chnage vite configuration for locating the dist folder

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,5 +5,9 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, "src")
+  root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true
+  }
 })
