cc3445c6a99c99eacf3cb10d81dc2c72c96b8665
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-29 21:27:24 +0800

    Add responsiveness for the home page

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -12,7 +12,7 @@ interface SelectionProps {
 
 const Selection = ({ index, src, url, callbackFn, children, disabled = false }: SelectionProps) => (
     !disabled ?
-        <NavLink className="flex w-2/3 flex-row items-center gap-10 py-3 px-2 rounded transition-all ease-in bg-transparent hover:bg-zinc-200"
+        <NavLink className="text-left flex w-2/3 flex-row items-center gap-10 py-3 px-2 rounded transition-all ease-in bg-transparent hover:bg-zinc-200"
             to={url}
             onMouseEnter={e => {
                 e.preventDefault();
@@ -24,12 +24,12 @@ const Selection = ({ index, src, url, callbackFn, children, disabled = false }: 
                 callbackFn(0);
             }}
         >
-            <figure className="w-12">
-                <img className="w-max" src={src} alt="icon" />
+            <figure>
+                <img className="max-w-none w-14 h-14" src={src} alt="icon" />
             </figure>
             <p className="font-bold text-base">{children}</p>
         </NavLink> :
-        <button className="flex w-2/3 flex-row items-center gap-10 py-3 px-2 rounded transition-all ease-in bg-transparent hover:bg-zinc-200 cursor-not-allowed"
+        <button className="text-left flex w-2/3 flex-row items-center gap-10 py-3 px-2 rounded transition-all ease-in bg-transparent hover:bg-zinc-200 cursor-not-allowed"
             onMouseEnter={e => {
                 e.preventDefault();
                 callbackFn(index);
@@ -40,8 +40,8 @@ const Selection = ({ index, src, url, callbackFn, children, disabled = false }: 
                 callbackFn(0);
             }}
         >
-            <figure className="w-12">
-                <img className="w-max" src={src} alt="icon" />
+            <figure>
+                <img className="max-w-none w-14 h-14" src={src} alt="icon" />
             </figure>
             <p className="font-bold text-base text-slate-500">{children}</p>
         </button>
@@ -61,15 +61,15 @@ export default function Main() {
     ];
 
     return (
-        <article className="flex flex-row h-screen">
-            <figure className="flex w-1/2 h-screen justify-center items-center">
-                <img className={`w-max ${currentIndex !== 0 ? "bg-cover h-screen" : ""}`} src={images[currentIndex]} alt="should be an image here" />
-            </figure>
+        <article className="flex flex-col md:flex-row h-36 py-10 md:p-0 md:h-screen">
+            <figure className={`flex w-1/2 bg-center ${currentIndex !== 0 ? "bg-cover": "bg-no-repeat"} hidden md:block md:h-screen`} style={{
+                backgroundImage: `url(${images[currentIndex]})`
+            }} />
             <section className="flex flex-auto flex-col gap-0 justify-center items-center">
-                <Selection index={1} src="/assets/college-school-icon.png" url="/colleges" callbackFn={i => setIndex(i)}>COLLEGES</Selection>
-                <Selection index={2} src="/assets/quote-request.png" url="/solicitation" callbackFn={i => setIndex(i)}>SOLICITATION</Selection>
-                <Selection index={3} src="/assets/yearbook.png" url="/yearbook-photos" callbackFn={i => setIndex(i)}>YEARBOOK PHOTOS</Selection>
-                <Selection index={4} src="/assets/yearbook-2.png" url="/yearbook-released" callbackFn={i => setIndex(i)} disabled={disabled}>YEARBOOK RELEASED</Selection>
+                <Selection index={1} src="/assets/college-school-icon.png" url="/section/colleges" callbackFn={i => setIndex(i)}>COLLEGES</Selection>
+                <Selection index={2} src="/assets/quote-request.png" url="/section/solicitation" callbackFn={i => setIndex(i)}>SOLICITATION</Selection>
+                <Selection index={3} src="/assets/yearbook.png" url="/section/yearbook-photos" callbackFn={i => setIndex(i)}>YEARBOOK PHOTOS</Selection>
+                <Selection index={4} src="/assets/yearbook-2.png" url="/section/yearbook-released" callbackFn={i => setIndex(i)} disabled={disabled}>YEARBOOK RELEASED</Selection>
                 {disabled &&
                     <p className="font-bold text-slate-500">(Please get the claim stub)</p>
                 }
