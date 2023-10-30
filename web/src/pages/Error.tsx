463ab105e678553df654886888abff7b7ea4fe54
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-30 16:15:13 +0800

    Refactor error page

--- web/src/pages/Error.tsx
+++ web/src/pages/Error.tsx
@@ -5,14 +5,17 @@ export default function ErrorPage() {
     console.error(error);
 
     return (
-        <section className="error-page flex flex-col justify-center items-center h-screen">
+        <section className="error-page flex flex-col justify-center items-center h-screen p-10 text-center">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
-            <p className="mt-5">Either the developer has yet to create this part of the page or you input an invalid address.</p>
-            <p>Consult Nicki Pecision for additional information</p>
-            <p className="mt-5">
-                <i>Error Message: {error.statusText || error.message}</i>
-            </p>
+            <section className="flex flex-col gap-5 mt-2">
+                <p>Either the developer has yet to create this part of the page or you input an invalid address.</p>
+                <p>Consult Nicki Pecision for additional information</p>
+                <p>
+                    <i>Error Message: {error.statusText || error.message}</i>
+                </p>
+
+            </section>
             <NavLink className="font-bold" to="/home" replace>Click this link to return home</NavLink>
         </section>
     );
