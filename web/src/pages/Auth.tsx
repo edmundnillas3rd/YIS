b67039b015947b4b903a3db2e7deae99e7764456
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-30 08:27:29 +0800

    Add tentative admin login and verification

--- web/src/pages/Auth.tsx
+++ web/src/pages/Auth.tsx
@@ -1,27 +1,67 @@
-import { SyntheticEvent } from "react";
+import { SyntheticEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import cjcLogo from "/assets/cjc-logo.png";
 
 export default function Auth() {
+    const [email, setEmail] = useState<string | null>(null);
+    const [password, setPassword] = useState<string | null>(null);
+    const [errMessage, setErrMessage] = useState<string | null>(null);
+
+    const adminEmail = import.meta.env.VITE_ADMIN_ACCOUNT;
+    const adminPassword = import.meta.env.VITE_BASE_ADMIN_PASSWORD;
+
     const navigate = useNavigate();
+
     const onSubmitHandler = (e: SyntheticEvent) => {
         e.preventDefault();
 
-        navigate("/home");
+        if (email === adminEmail && password === adminPassword) {
+            navigate("/home");
+        } else {
+            setErrMessage("Invalid email and password, please try again.");
+        }
     };
 
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <section className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img className="mx-auto h-32 w-auto" src={cjcLogo} alt="cor jesu logo" />
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Cor Jesu College<br/>Yearbook Information System</h2>
-            </div>
-
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Cor Jesu College<br />Yearbook Information System</h2>
+            </section>
+            <section className="flex p-2 flex-row justify-center items-center">
+                <p className={`text-red-400 text-sm font-bold my-2 h-2 ${errMessage ? "visible" : "invisible"}`}>{errMessage}</p>
+            </section>
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <form onSubmit={onSubmitHandler} method="POST">
-                    <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-t-md border-1 border-gray-300  border-b-gray-50 py-1.5 text-gray-700 shadow-sm ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" />
-                    <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-b-md border-1 border-gray-300  border-t-gray-50 py-1.5 text-gray-700 shadow-sm ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" />
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full rounded-t-md border-1 border-gray-300  border-b-gray-50 py-1.5 text-gray-700 shadow-sm ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="Email"
+                        onChange={e => {
+                            e.preventDefault();
+                            setErrMessage(null);
+                            setEmail(e.target.value);
+                        }}
+                    />
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full rounded-b-md border-1 border-gray-300  border-t-gray-50 py-1.5 text-gray-700 shadow-sm ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="Password"
+                        onChange={e => {
+                            e.preventDefault();
+                            setErrMessage(null);
+                            setPassword(e.target.value);
+                        }}
+                    />
                     <div className="mt-4">
                         <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Sign in</button>
                     </div>
