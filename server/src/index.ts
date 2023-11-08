656f2fa651d240e1bb8c54056bb15f3c02d3bf73
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-11-08 14:46:25 +0800

    Add user and solicitation endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,37 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import session from "express-session";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app: Express = express();
 
-import initializeDB, { query } from "./services/mysqldb";
+import initializeDB from "./services/mysqldb";
+import userRoute from "./routes/userRoute";
+import solicitationRoute from "./routes/solicitationRoute";
 
 const main = async () => {
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+
+    app.use(cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    }));
+
+    app.use(
+        session({
+            secret: process.env.SECRET as string,
+            resave: false,
+            saveUninitialized: false
+        })
+    );
+
     await initializeDB();
+
+    app.use("/users", userRoute);
+    app.use("/solicitation", solicitationRoute);
 
     app.listen(process.env.PORT || 3000, () => {
         console.info(`[server]: Listening on port ${process.env.PORT}`);
