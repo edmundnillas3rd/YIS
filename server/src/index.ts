816ee7fe27d2b8a3a5331f6a560250c3732beeef
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-11-08 09:27:57 +0800

    Add mysql configuration and connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,33 @@
 import express, { Express, Request, Response } from "express";
+import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app: Express = express();
 
 const main = async () => {
+
+    let db = null;
+
+    if (process.env.NODE_ENV !== "production") {
+        db = mysql.createConnection({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+        });
+    }
+
+    db?.connect((err) => {
+        if (err) {
+            console.error(err);
+        }
+
+        console.log("[server]: MySQL Connected");
+    });
+
     app.listen(process.env.PORT || 3000, () => {
-        console.info(`[server]: Listening on port ${process.env.PORT}`)
+        console.info(`[server]: Listening on port ${process.env.PORT}`);
     });
 };
 
