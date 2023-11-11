3606a6ecab7bd5181eb6b912dfb2ea2bf1d225ef
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-11-11 08:44:36 +0800

    Add routes for getting relevant club information about user

--- server/src/controllers/club.ts
+++ server/src/controllers/club.ts
@@ -10,3 +10,22 @@ export async function index(req: Request, res: Response) {
         organizations: organizations.rows
     });
 }
+
+export async function userClub(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const { rows } = await query(`
+        SELECT club_organization.club_organization_name as organization, club_position.club_position_name as position, YEAR(club.club_started) as 'Year Started', YEAR(club.club_ended) as 'Year Ended' FROM user
+        INNER JOIN club
+        ON club.user_id = user.user_id
+        INNER JOIN club_organization
+        ON club.club_organization_id = club_organization.club_organization_id
+        INNER JOIN club_position
+        ON club.club_position_id = club_position.club_position_id
+        WHERE user.user_id = ?;
+    `, [id]);
+
+    res.status(200).json({
+        rows
+    });
+}
--- server/src/routes/clubRoute.ts
+++ server/src/routes/clubRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router();
 
-import { index } from "../controllers/club";
+import { index, userClub } from "../controllers/club";
 
 router.get("/", index);
+router.get("/:id", userClub)
 
 export default router;
