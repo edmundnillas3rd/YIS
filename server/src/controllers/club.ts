7e8fd8a81e8906678744f73b3b6fe954c5b780b3
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-11-15 09:24:24 +0800

    Add route for getting all positions for user

--- server/src/controllers/club.ts
+++ server/src/controllers/club.ts
@@ -15,7 +15,7 @@ export async function userClub(req: Request, res: Response) {
     const { userID } = req.session;
 
     const { rows } = await query(`
-        SELECT club_organization.club_organization_name as organization, club_position.club_position_name as position, club.club_started as 'Year Started', club.club_ended as 'Year Ended' FROM user
+        SELECT club_organization.club_organization_id as club_id, club_organization.club_organization_name as organization, club_position.club_position_name as position, club.club_started as 'Year Started', club.club_ended as 'Year Ended' FROM user
         INNER JOIN club
         ON club.user_id = user.user_id
         INNER JOIN club_organization
@@ -30,11 +30,27 @@ export async function userClub(req: Request, res: Response) {
     });
 }
 
+export async function userClubInfo(req: Request, res: Response) {
+    const { userID } = req.session;
+    const { clubID } = req.params;
+
+    const { rows } = await query(`
+        SELECT club_position.club_position_name, club.club_started, club.club_ended FROM club
+        INNER JOIN club_position
+        ON club.club_position_id = club_position.club_position_id
+        WHERE club.user_id = ? AND club.club_organization_id = ?
+    `, [userID, clubID])
+
+    res.status(200).json({
+        userClubPositions: rows
+    })
+}
+
 export async function clubUserAdd(req: Request, res: Response) {
     const { userID } = req.session;
-    const { club, position, yearStart, yearEnd } = req.body;
+    const { club } = req.body;
 
-    // TODO: find if user already is in a club;
+    // find if user already is in a club
     const foundClub = await query(`
         SELECT user.user_id FROM user
         INNER JOIN club
@@ -47,14 +63,35 @@ export async function clubUserAdd(req: Request, res: Response) {
     }
 
     const ClubUUID = await query("SELECT UUID()");
-    const { rows } = await query("INSERT INTO club VALUES (?, ?, ?, ?, ?, ?)", [
+    const { rows } = await query("INSERT INTO club VALUES (?, ?, ?)", [
         ClubUUID.rows[0]['UUID()'],
         userID,
         club,
-        position,
-        yearStart,
-        yearEnd
     ]);
 
     res.status(200).end();
+}
+
+export async function clubUserPositionAdd(req: Request, res: Response) {
+    const { userID } = req.session;
+    const { club, position, yearStarted, yearEnded } = req.body;
+
+    const clubOrganizationRow = await query("SELECT club_organization.club_organization_id as club FROM club_organization WHERE club_organization.club_organization_name = ?", [club]);
+    const clubOrganizationID = clubOrganizationRow.rows[0].club;
+
+    const foundClubBelongTo = await query(`
+        SELECT club_position.club_position_id, club_position.club_position_name, user.user_id , CONCAT(user.user_first_name, " ", user.user_family_name, " ", user.user_middle_name) as full_name FROM user
+        INNER JOIN club
+        ON user.user_id = club.user_id
+        INNER JOIN club_position
+        ON club.club_position_id = club_position.club_position_id
+        WHERE user.user_id = ? AND club.club_organization_id = ?;
+    `, [userID, clubOrganizationID]);
+
+
+    if (foundClubBelongTo.rows.length > 0) {
+        return res.status(400).json({ error: "Another entry already exist "});
+    }
+
+
 }
--- server/src/routes/clubRoute.ts
+++ server/src/routes/clubRoute.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 const router = Router();
 
-import { index, userClub, clubUserAdd } from "../controllers/club";
+import { index, userClub, clubUserAdd, clubUserPositionAdd, userClubInfo } from "../controllers/club";
 
 router.get("/", index);
-router.get("/user-club", userClub)
+router.get("/user-club", userClub);
+router.get("/:clubID/user-club-info", userClubInfo);
 
 // POST
 router.post("/club-add", clubUserAdd);
+router.post("/position-add", clubUserPositionAdd);
 export default router;
