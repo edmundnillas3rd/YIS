b6e7b62ebc98a9c22d6b26fe70064decd996a5e9
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-12-14 22:39:53 +0800

    Refactor query to guarenteed to prevent dubplicates

--- server/src/controllers/yearbook.ts
+++ server/src/controllers/yearbook.ts
@@ -6,8 +6,8 @@ export async function index(req: Request, res: Response) {
         SELECT yearbook_status_id AS id, yearbook_status_name AS name FROM yearbook_status
     `);
 
-    const yearbookPhotos = await query(`
-        SELECT yearbook_photos.yearbook_photos_id AS id, CONCAT(user.user_first_name, ' ', user.user_family_name, ' ', user.user_middle_name, ' ', user.user_suffix) AS fullName, yearbook_status.yearbook_status_name AS yearbookStatus, COALESCE(DATE_FORMAT(yearbook_photos.yearbook_photos_date_released, '%m-%d-%Y'), 'N/A') AS dateReleased FROM yearbook_photos
+    let yearbookPhotos = await query(`
+        SELECT DISTINCT yearbook_photos.yearbook_photos_id AS id, CONCAT(user.user_first_name, ' ', user.user_family_name, ' ', user.user_middle_name, ' ', user.user_suffix) AS fullName, yearbook_status.yearbook_status_name AS yearbookStatus, COALESCE(DATE_FORMAT(yearbook_photos.yearbook_photos_date_released, '%m-%d-%Y'), 'N/A') AS dateReleased FROM yearbook_photos
         INNER JOIN user
         ON yearbook_photos.user_id = user.user_id
         INNER JOIN yearbook_status
